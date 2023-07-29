af6ef6ff561a2151313cbbd0cbfe7a0d604b5e93
Dennis <dennisjcarvalho@gmail.com>
2023-07-28 22:32:24 -0300

    feat: adicionado novo component e novas imagens

---
+++ src/app/components/card-list-pet/card-list-pet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-list-pet',
+  templateUrl: './card-list-pet.component.html',
+  styleUrls: ['./card-list-pet.component.css']
+})
+export class CardListPetComponent {
+
+}
